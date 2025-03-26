48889204cf66611e9aaf1e2ce5834ed7382f447a
Molly Du <modu@my.utexas.edu>
2025-03-26 16:45:44 -0500

    fix the function and all the tests to pass

--- my_code.py
+++ my_code.py
@@ -1,6 +1,10 @@
 import pytest
+import re
 
 def fix_phone_num(phone_num_to_fix):
+  phone_num_to_fix = re.sub(r'\W+', '', phone_num_to_fix)
+  if len(phone_num_to_fix) != 10 or not(phone_num_to_fix.isdigit()):
+    raise ValueError   
   # given "5125558823". Split the parts, then recombine and return
   area_code = phone_num_to_fix[0:3] # 512 (first three digits)
   three_part = phone_num_to_fix[3:6] # 555 (next three digits)
@@ -20,7 +24,8 @@ def test_fix_phone_num3():
   assert fix_phone_num("3216543333") == '(321) 654 3333'
 
 def test_fix_phone_num_hyphen_inputs():
-  assert fix_phone_num("555-442-98761") == '(555) 442 9876'
+  with pytest.raises(ValueError):
+    fix_phone_num("555-442-98761")
 
 def test_fix_phone_num_pre_formatted():
   assert fix_phone_num("(321) 654 3333") == "(321) 654 3333"
